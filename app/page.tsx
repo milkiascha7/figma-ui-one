8c725601d4fb0d7147bedc42cd7479bf46848818
milkiascha7 <mikehailu504@gmail.com>
2024-05-16 17:20:39 -0600

    "add locomotive for smooth scroll"

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,27 @@
+"use client"
+
 import Features from "@/components/features/features";
 import Footer from "@/components/footer/footer";
 import HeroSection from "@/components/herosection/hero-section";
 import Price from "@/components/price/price";
 import Testimonial from "@/components/testimonial/testimonial";
+import { useEffect } from "react";
+
+import LocomotiveScroll from 'locomotive-scroll';
 
 export default function Home() {
+  useEffect(() => {
+    (async () => {
+      const LocomotiveScroll = (await import('locomotive-scroll')).default;
+      const locomotiveScroll = new LocomotiveScroll();
+
+      // setTimeout(() => {
+      //   setIsLoading(false);
+      //   document.body.style.cursor = 'default';
+      //   window.scrollTo(0, 0);
+      // }, 2000);
+    })();
+  }, [])
   return (
     <main className="bg-brand-dark flex h-full flex-col items-center justify-between">
      <HeroSection />
